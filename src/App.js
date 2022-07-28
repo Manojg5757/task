3fbf90ac0d6d7052067a1235419dcc8234dda657
Manoj G <108245231+Manojg5757@users.noreply.github.com>
2022-07-28 17:15:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/js/bootstrap.bundle.js"
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [date,setDate] =useState("")
+  const[amount,setAmount] =useState("");
+  const[payment,setPayment]=useState("")
+  const[remark,setRemark] = useState("");
+
+  const[list,setList] = useState([]);
+  const handleSubmit =(e) =>{
+    e.preventDefault();
+    console.log(date,amount,payment,remark)
+    const data = {date,amount,payment,remark}
+    if(date,amount,payment,remark){
+      setList((ls)=>[...ls,data])
+      setDate("");
+      setAmount("")
+      setRemark("")
+    }
+  }
+
+
+
+ return (
+  <div>
+    <h1>React</h1>
+    <form onSubmit={handleSubmit} className="form-box">
+    <div className='inform'>
+    <label htmlFor="">Date:
+    <input type="date" name='date' value={date} onChange={(e) =>{setDate(e.target.value) }} style={{width:'200px'}}/>
+    </label>
+    <br />
+    <label htmlFor="">Amount:
+    <input type="number" name="amount" placeholder='Amount' value={amount} onChange={(e) => {setAmount(e.target.value)}} style={{width:'200px'}}/>
+    </label>
+    <br />
+    <label htmlFor="">Payment:
+    <select style={{width:'200px'}} name="payment" id="list" value={payment} onChange={(e) => {setPayment(e.target.value)}}>
+      <option>cash</option>
+      <option>Card</option>
+      <option>UPI</option>
+    </select></label><br />
+    <label htmlFor="">Remarks:
+    <input type="text" name="remark" placeholder='remark' value={remark} onChange={(e) => {setRemark(e.target.value)}} style={{width:'200px'}}/>
+    </label>
+    <br />
     </div>
-  );
+    <div className='buttons'>
+    <button>Cancel</button>
+    <button>Submit</button>
+    </div>
+    </form>
+    {
+      list.map((a) =><div>
+        <table className='table bordered'>
+          <tr>
+            <th>Date</th>
+            <th>Amount</th>
+            <th>Payment Method</th>
+            <th>Remarks</th>
+          </tr>
+          <tr>
+            <td>{a.date}</td>
+            <td>{a.amount}</td>
+            <td>{a.payment}</td>
+            <td>{a.remark}</td>
+          </tr>
+        </table>
+      </div>)
+    }
+  </div>
+ )
 }
 
 export default App;
